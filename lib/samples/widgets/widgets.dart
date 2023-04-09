e5cd63666864098905bfb6935ba526c180d830d2
yui <y129may9th@gmail.com>
2023-04-10 00:57:17 +0900

    feat: add rich text

---
+++ lib/samples/widgets/widgets.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widget_key/samples/widgets/rich_text.dart';
+
+class Widgets extends StatelessWidget {
+  const Widgets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Learning widgets',
+          style: TextStyle(color: Colors.grey[700]),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('rich text'),
+              onPressed: () => _onPressed(context, const RichTextWidget()),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Future<void> _onPressed(BuildContext context, Widget widget) async {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => widget,
+    ),
+  );
+}
